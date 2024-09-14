174e7e3ebc2ca9747a8ad3ebfacb6db86d7554f4
DjorkyBrooow <consignylu@cy-tech.fr>
2024-09-14 03:11:08 +0200

    Change images, get ideas for spy skills

---
+++ src/characters/classes/Spy.py
@@ -0,0 +1,14 @@
+from characters.Class import Class
+
+class Spy(Class):
+
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def skill_1(self) -> None:
+        # Largage de missiles (style CoD)
+        return super().skill_1()
+    
+    def skill_2(self) -> None:
+        # Invisibility
+        return super().skill_2()
