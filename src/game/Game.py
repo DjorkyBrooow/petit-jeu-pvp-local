bdba8f50e1fec19b330b3af3b4ebd5f023b8a295
DjorkyBrooow <consignylu@cy-tech.fr>
2024-10-04 02:08:05 +0200

    Added json file,  display of the main screen Starting to display the game screen

--- src/game/Game.py
+++ src/game/Game.py
@@ -2,7 +2,7 @@ from characters.Class import Class
 from characters.classes import *
 from game.Square import Square
 from game.static.Faction import Faction
-import os
+import os, curses, json
 
 class Game:
     
@@ -10,68 +10,67 @@ class Game:
     alliance_list: list[Class] = []
     horde_list: list[Class]= []
     map: 'Map'
+    data: dict
+    nb_alliance: int
+    nb_horde: int
     
     def __init__(self) -> None:
-        pass
+        self.data = Game.load_data()
+        self.map = Map(self.data["optionsText"][0]["default"], self.data["optionsText"][1]["default"])
+        self.nb_alliance = self.data["optionsText"][2]["default"]
+        self.nb_horde = self.data["optionsText"][3]["default"]
 
     def start_game(self) -> None:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("""Vous avez lancé une partie de JdR PvP.
-Deux équipes vont s'affronter, la première à terrasser l'autre gagne la partie.
-Voici les étapes pour commencer la partie :""")
-        self.chose_map()
         self.chose_characters()
-        character_list = self.alliance_list + self.horde_list
-        character_list.sort(key=lambda x: x.priority, reverse=True)
-        while self.game_in_progress():
-            for character in character_list:
-                if character.is_alive:
-                    character.play_turn()
-            pass
-
-    def chose_map(self) -> None:
-        res = False
-        while not res:
-            width = int(input("1. Choisissez la largeur de la carte (min 10 - max 25): "))
-            if width >= 10 and width <= 25:
-                res = True
-        res = False
-        while not res:
-            height = int(input("2. Choisissez la hauteur de la carte (min 5 - max 15): "))
-            if height >= 5 and height <= 15:
-                res = True
-        self.map = Map(width, height)
+        # self.character_list = self.alliance_list + self.horde_list
+        # self.character_list.sort(key=lambda x: x.priority, reverse=True)
+        # while self.game_in_progress():
+        #     for character in self.character_list:
+        #         if character.is_alive:
+        #             character.play_turn()
+        #     pass
     
     def chose_characters(self) -> None:
-        nb_alliance = int(input("3. Choisissez le nombre de personnages de l'Alliance (max 5): "))
-        print("4. Choisissez les classes des personnages de l'Alliance")
-        print(f"Les classes disponibles sont les suivantes : {', '.join(Class.AVAILABLE_CLASSES)}")
-        for i in range(nb_alliance):
-            res = False
-            while not res:
-                choice = input(f"Choisissez la classe du personnage {i+1} : ")
-                if choice.lower() in [x.lower() for x in Class.AVAILABLE_CLASSES]:
-                    class_ = globals()[choice.capitalize()]
-                    self.alliance_list.append(class_(Faction.ALLIANCE))
-                    res = True
-                else:
-                    print(f"""Choisissez une classe disponible.
-Les classes disponibles sont les suivantes : {', '.join(Class.AVAILABLE_CLASSES)}""")
+        charWidth = len(max(Class.AVAILABLE_CLASSES, key=len))
+        charHeight = len(Class.AVAILABLE_CLASSES) + 1
+        x = int(round((curses.COLS-1)/2) - round(charWidth/2))
+        y = int(round((curses.LINES-1)/2) - round(charHeight/2))
+        char_win = curses.newwin(charHeight, charWidth, y, x)
+        for i in range (len(Class.AVAILABLE_CLASSES)):
+            char_win.addstr(i, 0, Class.AVAILABLE_CLASSES[i])
+        char_win.refresh()
+        char_win.getch()
+
+
+#         nb_alliance = int(input("3. Choisissez le nombre de personnages de l'Alliance (max 5): "))
+#         print("4. Choisissez les classes des personnages de l'Alliance")
+#         print(f"Les classes disponibles sont les suivantes : {', '.join(Class.AVAILABLE_CLASSES)}")
+#         for i in range(nb_alliance):
+#             res = False
+#             while not res:
+#                 choice = input(f"Choisissez la classe du personnage {i+1} : ")
+#                 if choice.lower() in [x.lower() for x in Class.AVAILABLE_CLASSES]:
+#                     class_ = globals()[choice.capitalize()]
+#                     self.alliance_list.append(class_(Faction.ALLIANCE))
+#                     res = True
+#                 else:
+#                     print(f"""Choisissez une classe disponible.
+# Les classes disponibles sont les suivantes : {', '.join(Class.AVAILABLE_CLASSES)}""")
                     
-        nb_horde = int(input("5. Choisissez le nombre de personnages de la Horde (max 5): "))
-        print("6. Choisissez les classes des personnages de la Horde")
-        print(f"Les classes disponibles sont les suivantes : {', '.join(Class.AVAILABLE_CLASSES)}")
-        for i in range(nb_horde):
-            res = False
-            while not res:
-                choice = input(f"Choisissez la classe du personnage {i+1} : ")
-                if choice.lower() in  [x.lower() for x in Class.AVAILABLE_CLASSES]:
-                    class_ = globals()[choice.capitalize()]
-                    self.horde_list.append(class_(Faction.HORDE))
-                    res = True
-                else:
-                    print(f"""Choisissez une classe disponible.
-Les classes disponibles sont les suivantes : {', '.join(Class.AVAILABLE_CLASSES)}""")
+#         nb_horde = int(input("5. Choisissez le nombre de personnages de la Horde (max 5): "))
+#         print("6. Choisissez les classes des personnages de la Horde")
+#         print(f"Les classes disponibles sont les suivantes : {', '.join(Class.AVAILABLE_CLASSES)}")
+#         for i in range(nb_horde):
+#             res = False
+#             while not res:
+#                 choice = input(f"Choisissez la classe du personnage {i+1} : ")
+#                 if choice.lower() in  [x.lower() for x in Class.AVAILABLE_CLASSES]:
+#                     class_ = globals()[choice.capitalize()]
+#                     self.horde_list.append(class_(Faction.HORDE))
+#                     res = True
+#                 else:
+#                     print(f"""Choisissez une classe disponible.
+# Les classes disponibles sont les suivantes : {', '.join(Class.AVAILABLE_CLASSES)}""")
 
     def game_in_progress(self) -> bool:
         for elem in self.alliance_list:
@@ -100,6 +99,15 @@ Les classes disponibles sont les suivantes : {', '.join(Class.AVAILABLE_CLASSES)
         self.map.reset_map()
         for elem in self.character_list:
             self.map.square_list[(elem.x_coord, elem.y_coord)].content = elem.content
+
+    @staticmethod
+    def load_data() -> dict:
+        #load extradata.json
+        basePath = os.path.abspath(os.path.dirname(__file__))
+        f = open(basePath+"/extradata.json", encoding="utf-8")
+        data = json.load(f)
+        f.close()
+        return data
 
 
 class Map():
--- src/main.py
+++ src/main.py
@@ -1,43 +1,83 @@
-#from game.Game import Game
-#from characters.Class import Class
-#from characters.classes import *
+import time
+import traceback
+from game.Game import Game
+from characters.Class import Class
+from characters.classes import *
 import curses
-from curses import wrapper
-from time import sleep
 
-#game = Game()
 
-# game.start_game()
+def display_main_menu(data):
+    
+    title = data["titleText"]
+
+    #calculate where to place the title
+    titleWidth = len(max(title, key=len))
+    titleHeight = len(title)+1
+    x = int(round((curses.COLS-1)/2) - round(titleWidth/2))
+    y = int(round((curses.LINES-1)/2) - round(titleHeight/2))
+    #create window for title
+    titleWindow = curses.newwin(titleHeight, titleWidth, y, x)
+    # #add text to window
+    for i in range(0, len(title)):
+        titleWindow.addstr(i, 0, title[i])
+
+    mainMenuText = data["mainMenuText"]
+    x1 = int(round((curses.COLS-1)/2) - round(len(mainMenuText)/2))
+    y1 = y + titleHeight+ 1
+    mainMenuWindow1 = curses.newwin(1, len(mainMenuText)+1, y1, x1)
+    mainMenuWindow1.addstr(0, 0, mainMenuText)
+
+    mainMenuText2 = data["mainMenuText2"]
+    x2 = int(round((curses.COLS-1)/2) - round(len(mainMenuText2)/2))
+    y2 = y1 + 1
+    mainMenuWindow2 = curses.newwin(1, len(mainMenuText2) + 1, y2, x2)
+    mainMenuWindow2.addstr(0, 0, mainMenuText2)
+
+    titleWindow.refresh()
+    mainMenuWindow1.refresh()
+    mainMenuWindow2.refresh()
+
+    key = titleWindow.getkey()
+
+    titleWindow.clear()
+    mainMenuWindow1.clear()
+    mainMenuWindow2.clear()
+
+    return key
+
+def launch_game(stdscr):
+    stdscr.clear()
+    data = Game.load_data()
+    key = display_main_menu(data)
+
+    if key == 'x':
+        stdscr.clear()
+        text = data["goodbyeText"]
+        text2 = data["goodbyeText2"]
+        stdscr.addstr(int(round((curses.LINES-1)/2)), int(round((curses.COLS-1)/2) - round(len(text)/2)), text)
+        stdscr.addstr(int(round((curses.LINES-1)/2) + 1), int(round((curses.COLS-1)/2) - round(len(text2)/2)), text2)
+        stdscr.refresh()
+        time.sleep(3)
+    if key == 'c':
+        pass
+    else:
+        stdscr.clear()
+        game = Game()
+        game.data = data
+        game.start_game()
+        return
+    
+
 
 def main(stdscr):
-  stdscr.clear()
+    try:
+        launch_game(stdscr)
+        #when game ends quit curses
+        curses.endwin()
+    #quit curses and print exception if there was an error
+    except Exception:
+        curses.endwin()
+        traceback.print_exc()
   
-  game_pad = curses.newpad(curses.LINES-1, curses.COLS-1)
-  test = stdscr.getch() #a=97, A=65
-  game_pad.addstr(str(test))
-  for i in range(10):
-    stdscr.clear()
-    stdscr.refresh()
-    game_pad.refresh(0,0, 5,5+i, 10,10+i)
-    sleep(0.5)
-  # stdscr.getch()
-
-wrapper(main)
-"""
-characters = Class.AVAILABLE_CLASSES
-mob = []
-chars = ""
-print("CLASSES ORDERED BY max_hp")
-for elem in characters:
-    char = globals()[elem.capitalize()]
-    max_hp = char.max_hp
-    if max_hp not in mob:
-        mob += [max_hp]
-        chars +=(f"{max_hp.name} :")
-        for character in characters:
-            char = globals()[character.capitalize()]
-            if char.max_hp == max_hp:
-                chars += (f"- {character}")
-        chars += "\n"
-print (chars)
-"""
+if __name__ == '__main__':
+    curses.wrapper(main)
